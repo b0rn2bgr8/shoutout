c28ad43360eabfef32afeb7ef5d8315d11edd41b
b0rn2bgr8 <ajibikebamise@gmail.com>
2017-08-18 12:38:58 -0800

    Laptop changes,firebase cloud implementation

--- server.js
+++ server.js
@@ -8,6 +8,50 @@ var passport = require('passport');
 var mongoose = require('mongoose');
 
 var config = require('./config/secret');
+//google cloud storage
+/*var admin = require("firebase-admin");
+
+var serviceAccount = require("path/to/serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://shoutout-fdf97.firebaseio.com"
+});*/
+
+var mime = require('mime');
+const keyFilename="./shoutout.json"; //replace this with api key file
+const projectId = "shoutout-fdf97" //replace with your project id
+const bucketName = `${projectId}.appspot.com`;
+
+const gcs = require('@google-cloud/storage')({
+    projectId,
+    keyFilename
+});
+
+const bucket = gcs.bucket(bucketName);
+
+const filePath = `./package.json`;
+const uploadTo = `subfolder/package.json`;
+const fileMime = mime.lookup(filePath);
+
+bucket.upload(filePath, {
+    destination: uploadTo,
+    public: true,
+    metadata: { contentType: fileMime, cacheControl: "public, max-age=300" }
+}, function (err, file) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log(createPublicFileURL(uploadTo));
+});
+
+
+function createPublicFileURL(storageName) {
+    return `http://storage.googleapis.com/shoutout-fdf97/${encodeURIComponent(storageName)}`;
+
+}
+//////////////////////////////////////////////////////////////////////////
 
 mongoose.connect(config.database,function(err){
 	if(err){
